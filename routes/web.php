ea7b7404cb2af07acea72e716637bf3d8b95e4c8
sardor-abdusattorov <s.abdusattorov@acdf.uz>
2026-01-29 09:41:46 +0500

    added mcamara localisation

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,13 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'throttle:30,1'],
+    ],
+    function () {
+        Route::get('/', [HomeController::class, 'index'])->name('home');
+    }
+);
+
